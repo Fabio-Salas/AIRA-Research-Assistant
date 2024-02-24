02e4df80ef08f4bcef72571144a408673c951fe8
Fabio Manuel Salas <97650550+Fabio-Salas@users.noreply.github.com>
2024-02-24 12:22:08 -0500

    Update aira_code.py

--- aira_code.py
+++ aira_code.py
@@ -20,3 +20,7 @@ st.write("""
 # AIRA 
 Your AI-based Research Assistant
 """)
+
+
+st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, 
+                 on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
