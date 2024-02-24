f989183ede2d8ce5793a07b2cb86852310cb67e0
Fabio Manuel Salas <97650550+Fabio-Salas@users.noreply.github.com>
2024-02-24 13:17:16 -0500

    Update aira_code.py

--- aira_code.py
+++ aira_code.py
@@ -21,10 +21,15 @@ st.write("""
 Your AI-based Research Assistant
 """)
 
+with st.chat_message("user"):
+    st.write("Hola 👋 ¿Qué quieres saber de la literatura?")
+
 uploaded_files = st.file_uploader("Choose a PDF file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
     
+
+
 
